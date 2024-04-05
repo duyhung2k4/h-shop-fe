fb527f203a453a6ff01533db203d32cab2d38817
Hùng Nguyễn <nguyenduyhung04092004@gmail.com>
2024-04-05 16:16:13 +0700

    một chút giao diện

--- src/pages/detailShop/index.tsx
+++ src/pages/detailShop/index.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import { useGetDetailShopQuery } from "@/redux/api/shop.api";
-import { Group, LoadingOverlay, Text } from "@mantine/core";
+import { Button, Group, LoadingOverlay, Stack, Text } from "@mantine/core";
 import { useNavigate, useParams } from "react-router";
+import { IconLayoutGridAdd, IconSettings } from "@tabler/icons-react";
+import { useMediaQuery } from "@mantine/hooks";
 
 const ShopDetail: React.FC = () => {
     const { shop_id } = useParams();
     const navigation = useNavigate();
+    const matches = useMediaQuery("(max-width: 768px)")
 
-    if(shop_id === undefined) {
+    if (shop_id === undefined) {
         navigation("/me/shop/not-found");
     }
 
@@ -21,14 +24,24 @@ const ShopDetail: React.FC = () => {
         refetch();
     }, []);
 
-    if(isLoading) {
+    if (isLoading) {
         return <LoadingOverlay visible overlayProps={{ radius: "sm", blur: 2 }} />
     }
 
     return (
-        <Group>
-            <Text>{data?.data?.name || ""}</Text>
-        </Group>
+        <Stack>
+            <Group justify="space-between">
+                <Text>{data?.data?.name || ""}</Text>
+                <Group>
+                    <Button
+                        leftSection={matches ? undefined : <IconSettings/>}
+                    >{matches ? <IconSettings/> : "Chỉnh sửa shop"}</Button>
+                    <Button 
+                        leftSection={matches ? undefined : <IconLayoutGridAdd />}
+                    >{matches ? <IconLayoutGridAdd /> : "Tạo mới sản phẩm"}</Button>
+                </Group>
+            </Group>
+        </Stack>
     )
 }
 
